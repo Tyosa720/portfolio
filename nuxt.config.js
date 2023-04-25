ee2977a719cb9204e80a633a526599668bda3138
ThéophileLPE <92462938+Tyosa720@users.noreply.github.com>
2023-04-25 13:40:45 +0200

    clean

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,11 +45,6 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
   ],
-  router:{
-    base: '/portfolio'
-  },
-
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }

dabb50848bb261485fdce73ea2dd31490436a65a
ThéophileLPE <92462938+Tyosa720@users.noreply.github.com>
2023-04-26 21:38:49 +0200

    menubar

---
+++ components/sidebar.vue
@@ -0,0 +1,68 @@
+<template>
+<v-sheet
+    height="400"
+    class="overflow-hidden"
+    style="position: relative;"
+    dark
+  >
+        <v-btn
+        rounded
+        outlined
+        
+        icon
+        color=""
+        dark
+        @click.stop="drawer = !drawer"
+        >
+          <v-icon>
+            mdi-menu
+          </v-icon>
+        </v-btn>
+      
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      right
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Menu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+        ],
+      }
+    },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="dark fill-height">
-    <Profil_card></Profil_card>
+    <!-- <Profil_card></Profil_card> -->
+    <Sidebar></Sidebar>
   </div>
 </template>
 
 <script>
 import Profil_card from '../components/profil_card.vue';
+import Sidebar from '../components/sidebar.vue';
 export default {
     name: "IndexPage",
-    components: { Profil_card }
+    components: { Profil_card, Sidebar }
 }
 </script>
 

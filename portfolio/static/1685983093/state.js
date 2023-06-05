fd881675d35c3c3323e71e35c49ccde616fb247e
ThéophileLPE <92462938+Tyosa720@users.noreply.github.com>
2023-06-05 18:38:47 +0200

    Add files via upload

---
+++ portfolio/static/1685983093/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002Fportfolio\u002Fstatic\u002F1685983093",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002Fportfolio\u002F",cdnURL:a}}}}(null));
